066753e109b27882ceb95b47321ec3102030ba6a
Enrique FR <enriquefranciscoramirez95@gmail.com>
2021-08-26 12:54:54 -0500

    Fin sección 7

---
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { SettingsService } from 'src/app/services/settings.service';
+
+
+
+@Component({
+  selector: 'app-account-settings',
+  templateUrl: './account-settings.component.html',
+  styles: [
+  ]
+})
+export class AccountSettingsComponent implements OnInit {
+
+  /* public linkTheme = document.querySelector('#theme'); */
+  public links?: NodeListOf<Element>;
+
+  constructor( private settingsService: SettingsService ) { }
+
+  ngOnInit(): void {
+    this.links = document.querySelectorAll('.selector');
+    this.settingsService.checkCurrentTheme();
+  }
+
+  changeTheme( theme: string ){
+    this.settingsService.changeTheme( theme );
+  }
+
+  
+
+  
+
+}
